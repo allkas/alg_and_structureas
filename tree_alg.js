134126f720e7742fd6c4f5d96658800357cbb97f
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-10-14 18:31:14 +0300

    iter

--- tree_alg.js
+++ tree_alg.js
@@ -51,4 +51,23 @@ const recursive = (tree) => {
     return sum;
 };
 
-console.log(recursive(tree));
+// console.log(recursive(tree));
+
+const iteration = (tree) => {
+    if (!tree.length) {
+        return 0;
+    }
+    let sum = 0;
+    let stack = [];
+    tree.forEach(node => stack.push(node));
+    while (stack.length) {
+        const node = stack.pop();
+        sum += node.v;
+        if (node.c) {
+            node.c.forEach(child => stack.push(child));
+        }
+    }
+    return sum;
+};
+
+console.log(iteration(tree));
