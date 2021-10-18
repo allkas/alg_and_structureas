bd14d8f74aa5ad80fca914bc56a67ef53591efbf
Aleksey Karlov <50362944+allkas@users.noreply.github.com>
2021-10-18 10:35:11 +0300

    binary tree

---
+++ binary_tree.js
@@ -0,0 +1,64 @@
+class Binary_tree {
+    constructor() {
+        this.root = null
+    }
+
+    add(value) {
+        if (!this.root) {
+            this.root = new TreeNode(value)
+        } else {
+            let node = this.root;
+            let newNode = new TreeNode(value);
+            while (node) {
+                if (value > node.value) {
+                    if (!node.right) {
+                        break;
+                    }
+                    node = node.right;
+                } else {
+                    if (!node.left) {
+                        break;
+                    }
+                    node = node.left;
+                }
+            }
+            if (value > node.value) {
+                node.right = newNode;
+            } else {
+                node.left = newNode;
+            }
+        }
+    }
+
+    print(root = this.root) {
+        if (!root) {
+            return true;
+        }
+        console.log(root.value);
+        this.print(root.left);
+        this.print(root.right);
+    }
+}
+
+
+class TreeNode {
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null
+    }
+}
+
+const tree = new Binary_tree();
+tree.add(1);
+tree.add(3);
+tree.add(5);
+tree.add(13);
+tree.add(12);
+tree.add(2);
+tree.add(4);
+tree.add(7);
+tree.add(8);
+tree.add(14);
+
+tree.print();
